a51ad7f8198a150314d53f1bba43c272ff82b448
JU30N <joongwon.hong@gmail.com>
2025-03-14 14:24:37 +0100

    done

--- Quest_One.java
+++ Quest_One.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
 
+public class Quest_One {
+    public static void main(String[] args) {
 
+        long mincircumference = 1_000_000_000_000L;
+        
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("");
+        long amount_area_input = scanner.nextLong();
+        scanner.close(); 
+        
+        long sqrt_amount_area_input =  Math.round((long)Math.sqrt(amount_area_input));
+        
+        for (long x = 1; x <= sqrt_amount_area_input; x++){
+            //System.out.println(amount_area_input/x);
+            
+            if (amount_area_input % x == 0){
+                //System.out.println(y_value);
+                long circumference = 2*(x+(amount_area_input/x));//får omkrets 
+                //System.out.println("y: "+(amount_area_input/x )+ " x: "+x+" cir: "+circumference);
+                //System.out.println("mincir : "+mincircumference);
 
+                if (circumference < mincircumference){//om omkretsen ökar stop om den minskar fortsätt
+                    mincircumference = circumference;
+
+                }
+            }
+            
+        }
+        System.out.println(mincircumference);
+        
+    }
+}

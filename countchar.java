8205e1f918674e6116797361ae95251aa44af72a
JU30N <Joongwon.hong@gmail.com>
2025-03-18 20:50:25 +0100

    small things

--- countchar.java
+++ countchar.java
@@ -1,9 +1,3 @@
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Scanner;
-
 
 
 //uppgift att räkna saker i en sträng
@@ -16,21 +10,54 @@ import java.util.Scanner;
 //-> if key doent exist make a key with a value 1 
 
 //abfjgkdlfsa
-
-
-
-
-
-
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Scanner;
 
 public class countchar {
-
     public static void main(String[] args) {
         Dictionary<String, Integer> dAlphabet = new Hashtable<>();
-
         
 
+        //String keyToCheck = "Bob";
 
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("");
+        String string_input = scanner.next();
+        scanner.close(); 
+
+        //filter
+        String filterd_letters_string_input = string_input.replaceAll("[^a-zA-Z]","");
+        System.out.println(filterd_letters_string_input);
+
+
+        //length
+        int lenght_string_input = filterd_letters_string_input.length();
+        System.out.println(lenght_string_input);
+        
+        //if (dictionary.get(keyToCheck) != null) {
+          
+        insertLettersToDic(filterd_letters_string_input, dAlphabet, lenght_string_input);
+
+        int numberOfKeys = dAlphabet.size();
+        for (int i = 0; numberOfKeys > i; i++){
+            String key = dAlphabet.keys();
+            System.out.println();
+        }
 
     }
+
+    public static void insertLettersToDic(String str, Dictionary<String, Integer> dict, Integer length){
+        for (int i = 0; length > i; i++){
+            String character = String.valueOf(str.charAt(i));//changes the char to a string
+            
+            if (dict.get(character) == null){
+                dict.put(character, 1);
+            } else {
+                dict.put(character, dict.get(character) + 1);
+            }
+        }
+    } 
 }

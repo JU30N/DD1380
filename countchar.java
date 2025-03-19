5d755a57c862ca4519373a68251dbac20d6e71fc
JU30N <joongwon.hong@gmail.com>
2025-03-19 10:31:43 +0100

    school 10:31

--- countchar.java
+++ countchar.java
@@ -14,13 +14,14 @@ import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class countchar {
     public static void main(String[] args) {
-        Dictionary<String, Integer> dAlphabet = new Hashtable<>();
+        //variables
+        TreeMap<String,Integer> countAlfa;
         
 
-        //String keyToCheck = "Bob";
 
         
         Scanner scanner = new Scanner(System.in);
@@ -28,28 +29,30 @@ public class countchar {
         String string_input = scanner.next();
         scanner.close(); 
 
+        countAlfa = new TreeMap<String, Integer>();
+
         //filter
         String filterd_letters_string_input = string_input.replaceAll("[^a-zA-Z]","");
+        String filterd_to_lower_case_string_input = filterd_letters_string_input.toLowerCase();
         System.out.println(filterd_letters_string_input);
+        System.out.println(filterd_to_lower_case_string_input);
 
 
         //length
-        int lenght_string_input = filterd_letters_string_input.length();
+        int lenght_string_input = filterd_to_lower_case_string_input.length();
         System.out.println(lenght_string_input);
         
-        //if (dictionary.get(keyToCheck) != null) {
           
-        insertLettersToDic(filterd_letters_string_input, dAlphabet, lenght_string_input);
+        insertLettersToDic(filterd_to_lower_case_string_input, countAlfa, lenght_string_input);
 
-        int numberOfKeys = dAlphabet.size();
-        for (int i = 0; numberOfKeys > i; i++){
-            String key = dAlphabet.keys();
-            System.out.println();
+        System.out.println(countAlfa);
+        for (String word : countAlfa.keySet()) {//word gets a to be a key from the countAlfa 
+            System.out.println(word + " " + countAlfa.get(word));//thus for each key int countAlfa print the value of the corrispoding key
         }
 
     }
 
-    public static void insertLettersToDic(String str, Dictionary<String, Integer> dict, Integer length){
+    public static void insertLettersToDic(String str, TreeMap<String, Integer> dict, Integer length){
         for (int i = 0; length > i; i++){
             String character = String.valueOf(str.charAt(i));//changes the char to a string
             

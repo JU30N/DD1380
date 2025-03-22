fb00be49acb56bd77429fccc519579282c0efbd2
JU30N <Joongwon.hong@gmail.com>
2025-03-22 13:35:25 +0100

    testing out

--- decomp.java
+++ decomp.java
@@ -3,14 +3,44 @@
 //  0   4   7  10  13 16
 //[sju sjö sju ka sjö män]
 
-//[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-//[s,j,u, ,s,j,ö,s,j,u,k,a, ,s ,j ,ö ,m ,ä ,n]
+//[0 ,1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10,11,12,13,14,15,16,17,18,19,20]
+//[s ,j ,u ,  ,s ,j ,ö ,s ,j ,u ,k ,a ,  ,s ,j ,ö ,m ,ä ,n]
 
 //input "sju: 0 7; sjö: 4 13; ka 10; män: 16"
 //take each sju: 0 7; -> list
 //add s, j ,u from 0 to 2 and 7 to 9
 //print out the list
 
+//second idea
+//array [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// sju put in in 0
+// length of sju is 3 so on so 
+//[sju,1,2,3,sjö,5,6,sju,8,9,ka,11,12,sjö,14,15,män,17,18,19,20]
+// sju,1,2 is one word because sju is 3 letters 
+// sjö,5,6
+// sju,8,9
+// ka,11
+// sjö,14,15
+// män,17,18
+// thus 3, 12 doesnt have any letters therfore become spaces
+
+import java.util.Scanner;
+
+public class decomp {
+    public static void main(String[] args ) {
+        //var
+        String input = "sju: 0 7; sjö: 4 13; ka: 10; män: 16";
+
+        //dela upp ;
+        String[] input_parts = input.split(";");
+        
+        for(int i = 0; i < input_parts.length; i++){
+            System.out.println(input_parts[i]);
+        }
 
 
+
+
+    }
+}
 

913d07aacd8c4e5727ccaf3d33b46afecf2a6345
JU30N <Joongwon.hong@gmail.com>
2025-03-22 12:42:51 +0100

    idea to de comp

--- decomp.java
+++ decomp.java
@@ -1,1 +1,16 @@
 
+//sju: 0 7; sjö: 4 13; ka 10; män: 16"
+//  0   4   7  10  13 16
+//[sju sjö sju ka sjö män]
+
+//[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+//[s,j,u, ,s,j,ö,s,j,u,k,a, ,s ,j ,ö ,m ,ä ,n]
+
+//input "sju: 0 7; sjö: 4 13; ka 10; män: 16"
+//take each sju: 0 7; -> list
+//add s, j ,u from 0 to 2 and 7 to 9
+//print out the list
+
+
+
+
